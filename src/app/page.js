3e76389ef45fc324bc518e0c089ffc3644f58833
cj <cjcorc10@gmail.com>
2026-03-25 12:29:35 -0500

    saving progress for work on different device

--- src/app/page.js
+++ src/app/page.js
@@ -12,59 +12,78 @@ export default function Home() {
   const scrollRef = useRef(null)
   const counterRef = useRef(null)
   const imagesRef = useRef([])
+  const imageContainer = useRef(null)
   const done = useRef(false);
   const addToRefs = (e) => {
     if(e && !imagesRef.current.includes(e)) imagesRef.current.push(e)
   }
 
+  const setImages = (images) => {
+    const numImages = images.length
+    gsap.to(images, {
+      translateY: (i) => `${i*-30}%`,
+      translateZ: (i) => (i ? `${(i-1)*-10}px` : `${numImages*-10}px`),
+    })
+  }
+
   useGSAP(() => {
     if(!scrollRef) return
     const images = imagesRef.current
-    gsap.set(images, {
-      y: (i) => (i == images.length-1 ? 50 : i*-50),
-      translateZ: (i) => `${i*-50}px`,
-      transformOrigin: (i) => 'top center'
-    })
+    const numImages = images.length
+    if(!numImages) return
 
-  
-    const tl = gsap.timeline({
-      scrollTrigger: {
+   
+      const scroll = ScrollTrigger.create({
         trigger: scrollRef.current,
         start: "top top",
         end: "+=1600px",
         pin: true,
         scrub: 1,
         markers: true,
+        // onEnter: () => setImages(images),  
         onUpdate: (self) => {
           const {progress} = self;
+          const steps = images.length - 1
+          const segmentLength = 1 / steps
+
+      
+          images.forEach((img, i) => {
+            const currentCard = img;
+            const nextCard = images[i+1]
+            
+            const startProgress = i * segmentLength
+            const endProgress = (i+1) * segmentLength
+
+            let localProgress = (progress-startProgress) / segmentLength
+            localProgress = gsap.utils.clamp(0, 1, localProgress)
+            if(progress > startProgress && progress < endProgress) {
+              gsap.to(images, {
+                translateY: (k) => images[k] != nextCard ? `${k * -30}%` : '30%',
+                translateZ: (k) => `${k*-30}px`,
+              })  
+
+            }
+          })
 
         }
-
-      }
-    });
-
-    images.map((image, i) => {
-      const lastImage = images[images.length-1]
-      // if(!lastImage) return
-      tl.to(images, {
-        y: (i) => (i == images.length-1 ? 50 : i*-50),            
-        translateZ: (i) => `${i*-50}px`,
       })
-      images.unshift(images.pop())
-    })
-
 
   }, {scope: scrollRef})
   return (
+    <>
+    <Filler />
     <main className={styles.main}>
       <div ref={counterRef} className={styles.counter}>0</div>
       <div ref={scrollRef} className={styles.scrollContainer}>
-        <div className={styles.imageContainer}>
+        <div ref={imageContainer} className={styles.imageContainer}>
           {[...Array(4)].map((_,i) => 
             <div ref={addToRefs} key={i} className={styles.image} />
           )}
         </div>
       </div>
     </main>
+          </>
   );
 }
+
+const Filler = () => <div style={{height: '50vh', width: '100vw'}} />
