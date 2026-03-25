82ef7cfc375ec744cdc904b55036e0abf9952cc6
cj <cjcorc10@gmail.com>
2026-03-25 17:25:21 -0500

    completed animation by onUpdate to control entire animation. config still needs to be added

--- src/app/page.js
+++ src/app/page.js
@@ -1,89 +1,108 @@
-'use client'
-import styles from './page.module.scss'
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { ScrollTrigger } from 'gsap/all';
-import { useRef } from 'react';
+"use client";
+import styles from "./page.module.scss";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
+const FOCUSED_IMAGES = 2;
+const GAP = 30;
 
 export default function Home() {
-
-  const scrollRef = useRef(null)
-  const counterRef = useRef(null)
-  const imagesRef = useRef([])
-  const imageContainer = useRef(null)
-  const done = useRef(false);
+  const scrollRef = useRef(null);
+  const counterRef = useRef(null);
+  const imagesRef = useRef([]);
+  const imageContainer = useRef(null);
   const addToRefs = (e) => {
-    if(e && !imagesRef.current.includes(e)) imagesRef.current.push(e)
-  }
+    if (e && !imagesRef.current.includes(e))
+      imagesRef.current.push(e);
+  };
 
-  const setImages = (images) => {
-    const numImages = images.length
-    gsap.to(images, {
-      translateY: (i) => `${i*-30}%`,
-      translateZ: (i) => (i ? `${(i-1)*-10}px` : `${numImages*-10}px`),
-    })
-  }
+  const getCards = (images, index) => {
+    if (index === images.length - 1) return images.slice(1, index);
+    return [
+      ...images.slice(index + FOCUSED_IMAGES),
+      ...images.slice(0, index),
+    ];
+  };
 
-  useGSAP(() => {
-    if(!scrollRef) return
-    const images = imagesRef.current
-    const numImages = images.length
-    if(!numImages) return
+  useGSAP(
+    () => {
+      if (!scrollRef) return;
+      const images = imagesRef.current;
+      const numImages = images.length;
+      if (!numImages) return;
 
-   
-      const scroll = ScrollTrigger.create({
-        trigger: scrollRef.current,
-        start: "top top",
-        end: "+=1600px",
-        pin: true,
-        scrub: 1,
-        markers: true,
-        // onEnter: () => setImages(images),  
-        onUpdate: (self) => {
-          const {progress} = self;
-          const steps = images.length - 1
-          const segmentLength = 1 / steps
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: scrollRef.current,
+          start: "top top",
+          end: "+=1600px",
+          pin: true,
+          scrub: 1,
+          markers: true,
+          onUpdate: (self) => {
+            const { progress } = self;
+            const steps = images.length;
+            const segmentLength = 1 / steps;
 
-      
-          images.forEach((img, i) => {
-            const currentCard = img;
-            const nextCard = images[i+1]
-            
-            const startProgress = i * segmentLength
-            const endProgress = (i+1) * segmentLength
+            images.forEach((img, i) => {
+              const frontCards = [img, images[i + 1] || images[0]];
+              const backgroundCards = getCards(images, i);
 
-            let localProgress = (progress-startProgress) / segmentLength
-            localProgress = gsap.utils.clamp(0, 1, localProgress)
-            if(progress > startProgress && progress < endProgress) {
-              gsap.to(images, {
-                translateY: (k) => images[k] != nextCard ? `${k * -30}%` : '30%',
-                translateZ: (k) => `${k*-30}px`,
-              })  
+              const startProgress = i * segmentLength;
+              const endProgress = (i + 1) * segmentLength;
 
-            }
-          })
+              let localProgress =
+                (progress - startProgress) / segmentLength;
+              localProgress = gsap.utils.clamp(0, 1, localProgress);
 
-        }
-      })
-
-  }, {scope: scrollRef})
+              if (
+                progress > startProgress &&
+                progress < endProgress
+              ) {
+                gsap.to(frontCards, {
+                  translateY: (k) => (k === 0 ? `0%` : `${GAP}%`),
+                  translateZ: (k) =>
+                    k ? `${numImages * -GAP}px` : 0,
+                });
+                gsap.to(backgroundCards, {
+                  translateY: (k) =>
+                    `${(numImages - FOCUSED_IMAGES - k) * -GAP}%`,
+                  translateZ: (k) =>
+                    `${(numImages - FOCUSED_IMAGES - k) * -GAP}px`,
+                });
+                gsap.to(imageContainer.current, {
+                  translateY: localProgress * -150,
+                });
+              }
+            });
+          },
+        },
+      });
+    },
+    { scope: scrollRef },
+  );
   return (
     <>
-    <Filler />
-    <main className={styles.main}>
-      <div ref={counterRef} className={styles.counter}>0</div>
-      <div ref={scrollRef} className={styles.scrollContainer}>
-        <div ref={imageContainer} className={styles.imageContainer}>
-          {[...Array(4)].map((_,i) => 
-            <div ref={addToRefs} key={i} className={styles.image} />
-          )}
+      <Filler />
+      <main className={styles.main}>
+        <div ref={counterRef} className={styles.counter}>
+          0
         </div>
-      </div>
-    </main>
-          </>
+        <div ref={scrollRef} className={styles.scrollContainer}>
+          <div ref={imageContainer} className={styles.imageContainer}>
+            {[...Array(4)].map((_, i) => (
+              <div ref={addToRefs} key={i} className={styles.image} />
+            ))}
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
 
-const Filler = () => <div style={{height: '50vh', width: '100vw'}} />
+const Filler = () => (
+  <div style={{ height: "50vh", width: "100vw" }} />
+);
